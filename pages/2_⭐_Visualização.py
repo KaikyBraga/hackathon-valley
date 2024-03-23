e883169e7b695031022aede7660ba853ae248851
samucaaaaaa <mucaspeedpvp@gmail.com>
2024-03-23 17:17:18 -0300

    Adicionando botão de download

---
+++ pages/2_⭐_Visualização.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Visualização")
+
+#with open("styles.css") as f:
+    #st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)  
+
+with st.container():
+    st.title("Visualizações dos Dados")
+    st.write("...")
+
+  
